0a3e5de0ca3413d5e394f2eb4d526ec63103ee24
Vasily Novosad <delhsmail@gmail.com>
2021-03-18 14:15:06 +0500

    Fix of incorrect context injection

--- src/middleware-factory.ts
+++ src/middleware-factory.ts
@@ -4,25 +4,16 @@ import { RedisMiddlewareFactory } from '@via-profit-services/redis';
 import redisFactory from './redis-factory';
 
 const middlewareFactory: RedisMiddlewareFactory = (config) => {
-
-  const pool: ReturnType<Middleware> = {
-    context: null,
-  };
-
-  const middleware: Middleware = (props) => {
-
-    if (pool.context !== null) {
-      return pool;
-    }
-
-    const { context } = props;
+  const middleware: Middleware = ({ context }) => {
     const { logger } = context;
 
     try {
-      pool.context = context;
-      pool.context.redis = redisFactory(config, logger.server);
+      context.redis = context.redis ?? redisFactory(config, logger.server);
 
-      return pool;
+      return {
+        context,
+      }
+
     } catch (err) {
       throw new ServerError('Failed to create Redis instance', { err })
     }
